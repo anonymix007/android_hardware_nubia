192ccf227687d0f84787076de1f2fceded6d31c3
anonymix007 <48598263+anonymix007@users.noreply.github.com>
2025-03-11 18:23:19 +0300

    fingeprint: Use stock UDFPS layer Z masks

--- aidl/biometrics/fingerprint/UdfpsExtension.cpp
+++ aidl/biometrics/fingerprint/UdfpsExtension.cpp
@@ -6,12 +6,16 @@
 
 #include <compositionengine/UdfpsExtension.h>
 
+#define ZTE_UDFPS_FOD_MASK 0x20000000
+#define ZTE_UDFPS_HBM_MASK 0x40000000
+#define ZTE_UDFPS_AOD_MASK 0x80000000
+
 uint32_t getUdfpsDimZOrder(uint32_t z) {
-    return 0x41000005;
+    return z | ZTE_UDFPS_HBM_MASK;
 }
 
 uint32_t getUdfpsZOrder(uint32_t z, bool touched) {
-    return touched ? 0x41000033 : z;
+    return touched ? (z | ZTE_UDFPS_FOD_MASK) : z;
 }
 
 uint64_t getUdfpsUsageBits(uint64_t usageBits, bool /* touched */) {
